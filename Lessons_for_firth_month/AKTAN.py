be919c90917745f634ba3c83d5c2f4c3fb021779
Bakyt0v <Amantur470@gmail.com>
2022-02-16 14:37:56 +0600

    first commit

---
+++ Lessons_for_firth_month/AKTAN.py
@@ -0,0 +1,14 @@
+word = input('Введите любое предложение:')
+letters = 'A'
+s = 0
+
+while len(word)>0:
+    if word[0]= letters
+        s+=1
+    word = word[1:]
+    else
+print(s)
+    break
+
+
+
