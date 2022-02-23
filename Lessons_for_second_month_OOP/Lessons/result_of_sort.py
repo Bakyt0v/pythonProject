400e33f566f36cd746e4a7f455d08865ebef50cd
Bakyt0v <Amantur470@gmail.com>
2022-02-23 15:05:58 +0600

    first commit

--- Lessons_for_second_month_OOP/Lessons/result_of_sort.py
+++ Lessons_for_second_month_OOP/Lessons/result_of_sort.py
@@ -11,6 +11,9 @@ print(f'Unsorted list is: {UNSORTED_LIST}')
 bubble_sort(UNSORTED_LIST)
 print(f'Sorted list by Bubble sort: {UNSORTED_LIST}')
 
+env.read_envfile('unsorted_list.env')
+UNSORTED_LIST = list(os.environ.get('UNSORTED_LIST'))
+
 print(f'Unsorted list is: {UNSORTED_LIST}')
 selection_sort(UNSORTED_LIST)
 print(f'Sorted by Selected sort: {UNSORTED_LIST}')
