e67305537bea52f4bc92f37872581d6a851d4ce3
Bakyt0v <Amantur470@gmil.com>
2022-03-09 18:55:44 +0600

    Delete hw.db

--- Lessons_for_second_month_OOP/awd.py
+++ Lessons_for_second_month_OOP/awd.py
@@ -1,150 +1,244 @@
-import random
+from aiogram import Bot, executor, Dispatcher, types
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup,ParseMode
+from decouple import config
+
+TOKEN = config('TOKEN')
+bot = Bot(TOKEN)
+dp = Dispatcher(bot=bot)
+
+@dp.message_handler(commands=['start'])
+async def hello(message: types.Message):
+    await bot.send_message(message.chat.id, f'Hello i am the new bot from master, if u wanna see all the commands just text /help {message.from_user.full_name}')
+
+@dp.message_handler(commands=['help'])
+async def hello(message: types.Message):
+    await bot.send_message(message.chat.id, f'/help-- to see all commands'
+                                            f'/start-- to see info about pythot'
+                                            f'/quiz-- to see example quiz'
+                                            f'/problem-- to see class work tasks'
+                                            f'/solve_task-- to see home work from student {message.from_user.full_name}')
 
 
 
-class GameEntity:
-    def __init__(self, name, health, damage):
-        self.__name = name
-        self.__health = health
-        self.__damage = damage
-
-    @property
-    def health(self):
-        return self.__health
-
-    @health.setter
-    def health(self, value):
-        self.__health = value
-
-    @property
-    def damage(self):
-        return self.__damage
-
-    @damage.setter
-    def damage(self, value):
-        self.__damage = value
-
-    # Ссылка на список и ссылка
-    def hit(self, boss, heroes):
-        pass
-
-    def __str__(self):
-        return f'Name: {self.__name} Health: {self.health} Damage: {self.damage}'
- # __ health обращение к конструктору health обпащение к гет
+@dp.message_handler(commands=['quiz'])
+async def poll(message: types.Message):
+    question = 'By whom invented Python'
+    answer = ['Harry Potter', 'Piter', 'Amantur', 'Aktan']
+    await bot.send_poll(message.chat.id,
+                        question=question,
+                        options=answer,
+                        is_anonymous=False,
+                        type='quiz',
+                        correct_option_id=2,
+                        open_period=10,
+                        explanation='You are looser',
+                        explanation_parse_mode=ParseMode.MARKDOWN_V2,
+                        )
 
 
-class Boss(GameEntity):
-    def __init__(self, name, health, damage):
-        GameEntity.__init__(self, name, health, damage)
 
-    def hit(self, boss, heroes):
-        for hero in heroes:
-            if boss.health > 0 and hero.health > 0:
-                hero.health = hero.health - boss.damage
-                if hero.health <= 0:
-                    hero.health = 0
+@dp.message_handler(commands=['problem'])
+async def problem(message: types.Message):
+    markup = InlineKeyboardMarkup()
+    button_call_1 = InlineKeyboardButton('Next task',
+                                         callback_data='button_call_1')
+    markup.add(button_call_1)
+    photo = open('../media/summfloatinteger.jpg', 'rb')
+    answer1 = ['0.0', '4', '5', '8.0', 'Error']
+    question1 = 'Output of this task in picture above'
+    await bot.send_photo(
+        message.chat.id,
+        photo=photo)
 
-
-class Hero(GameEntity):
-    def __init__(self, name, health, damage, super_ability):
-        GameEntity.__init__(self, name, health, damage)
-        self.__super_ability = super_ability
-
-    @property
-    def super_ability(self):
-        return self.__super_ability
-
-    def apply_super_ability(self, boss, heroes):
-        pass
-
-    def hit(self, boss):
-        if boss.health > 0 and self.health > 0:
-            boss.health = boss.health - self.damage
-            if boss.health <= 0:
-                boss.health = 0
-
-class Warrior(Hero):
-    def __init__(self, name, health, damage):
-        Hero.__init__(self, name, health, damage, 'CRITICAL_DAMAGE')
-
-    def apply_super_ability(self, boss, heroes):
-        pass
-
-class Medic(Hero):
-    def __init__(self, name, health, damage, heal_points):
-        Hero.__init__(self, name, health, damage, 'HEAL')
-        self.__heal_points = heal_points
-
-    def apply_super_ability(self, boss, heroes):
-        if boss.health > 0 and self.health > 0:
-            for hero in heroes:
-                if hero.health > 0 and self != hero:
-                    hero.health = hero.health + self.__heal_points
+    await bot.send_poll(
+        message.chat.id,
+        question = question1,
+        options=answer1,
+        correct_option_id=0,
+        is_anonymous=False,
+        type='quiz',
+        open_period=20,
+        reply_markup=markup,
+        explanation = 'It is so easy so you have only 20 seconds',
+        explanation_parse_mode=ParseMode.MARKDOWN_V2
+    )
 
 
-class Magic(Hero):
 
-    def __init__(self, name, health, damage):
-        Hero.__init__(self, name, health, damage, 'BOOST')
+@dp.callback_query_handler(lambda func: func.data == 'button_call_1')
+async def problem2(call: types.CallbackQuery):
+    markup1 = InlineKeyboardMarkup()
+    button_call_2 = InlineKeyboardButton('Next task',
+                                         callback_data='button_call_2')
+    markup1.add(button_call_2)
 
-    def apply_super_ability(self, boss, heroes):
-        for hero in heroes:
-            if hero.health > 0:
-                hero.damage = hero.damage + 5
-
-
-# функции
-
-round_number = 0
-
-
-def print_statistics(boss, heroes):
-    print(f'{round_number} ROUND -----------------')
-    print(boss)
-    for hero in heroes:
-        print(hero)
-
-
-def is_game_finished(boss, heroes):
-    if boss.health <= 0:
-        print('Heroes won')
-        return True
-
-    all_heroes_dead = True
-    for hero in heroes:
-        if hero.health > 0:
-            all_heroes_dead = False
-            break
-    if all_heroes_dead:
-        print('Boss Won!!!')
-    return all_heroes_dead
+    photo1 = open('../media/listremoveappend.jpg', 'rb')
+    answer2 = ['1', '2', '3', '4', '5', '6', '7']
+    question2 = 'Output of this task in picture above:'
+    await bot.send_photo(
+        call.message.chat.id,
+        photo=photo1)
+    await bot.send_poll(
+        call.message.chat.id,
+        question=question2,
+        options=answer2,
+        correct_option_id=2,
+        is_anonymous=False,
+        type='quiz',
+        open_period=15,
+        reply_markup=markup1,
+        explanation='Little bit harder than previue task',
+        explanation_parse_mode=ParseMode.MARKDOWN_V2,
+    )
 
 
-def round(boss, heroes):
-    global round_number
-    round_number += 1
-    boss.hit(boss, heroes)
-    for hero in heroes:
-        hero.hit(boss)
-        hero.apply_super_ability(boss, heroes)
-    print_statistics(boss, heroes)
+
+@dp.callback_query_handler(lambda func: func.data == 'button_call_2')
+async def problem3(call: types.CallbackQuery):
+    photo2 = open('../media/homework.jpg', 'rb')
+    answer3 = ['0','1', '2', '3', '4', '5']
+    question3 = 'Output of this task in picture above:'
+    await bot.send_photo(
+        call.message.chat.id,
+        photo=photo2)
+    await bot.send_poll(
+        call.message.chat.id,
+        question=question3,
+        options=answer3,
+        correct_option_id=0,
+        is_anonymous=False,
+        type='quiz',
+        open_period=59,
+        explanation='You can try solve this task in Pycharm',
+        explanation_parse_mode=ParseMode.MARKDOWN_V2
+    )
 
 
-def start():
-    # вызов контруктора внутри функции
-    boss = Boss('OverLord', random.randint(700, 1000), 50)
+markup0 = InlineKeyboardMarkup()
+button1 = InlineKeyboardButton('It is easy to me', callback_data='button1')
+button2 = InlineKeyboardButton('It is so hard to me', callback_data='button2')
+button3 = InlineKeyboardButton('i am tired i just wanna see the result', callback_data='button3')
+button4 = InlineKeyboardButton('Tasks for lazy HUMANs', callback_data='button4')
 
-    warrior = Warrior('Soup', random.randint(230, 300), 15)
+markup0.add(button1,button2,button3,button4)
 
-    medic = Medic('Dog', random.randint(180, 260), 5, 15)
-    medic1 = Medic('pavel', random.randint(250, 260), 15, 5)
-    magic = Magic('Samuel', random.randint(250, 300), 20)
-    heroes = [warrior, medic, medic1, magic]
+@dp.message_handler(commands=['solve_task'])
+async def process_start_command(message: types.Message):
+    photo_1 = open('../media/home_work_solved/task1.png', 'rb')
+    await bot.send_photo(
+        message.chat.id,
+        photo=photo_1
+    )
 
-    print_statistics(boss, heroes)
+    await message.reply('Could you solve the task above in picture?', reply_markup=markup0)
 
-    while not is_game_finished(boss, heroes):
-        round(boss, heroes)
+@dp.callback_query_handler(lambda func: func.data == 'button2')
+async def solved_task(call: types.CallbackQuery):
+    photo4 = open('../media/home_work_solved/solvedscreen.png','rb')
+    await bot.send_photo(
+        call.message.chat.id,
+        photo=photo4
+    )
+
+@dp.callback_query_handler(lambda func: func.data == 'button3')
+async def picture(call: types.CallbackQuery):
+    photo4 = open('../media/home_work_solved/picture.jpeg','rb')
+    await bot.send_photo(
+        call.message.chat.id,
+        photo=photo4
+    )
+
+@dp.callback_query_handler(lambda func: func.data == 'button4')
+async def solve_task(call: types.CallbackQuery):
+    markup3 = InlineKeyboardMarkup()
+    answer4 = ['0.0','1','2.0','4.0','error']
+    question4 = 'Result of int2 + float2'
+    button5 = InlineKeyboardButton('Next_task', callback_data='button5')
+    markup3.add(button5)
+
+    await bot.send_poll(
+        call.message.chat.id,
+        question=question4,
+        options=answer4,
+        correct_option_id=3,
+        is_anonymous=False,
+        type='quiz',
+        explanation='only one of these options are true',
+        explanation_parse_mode=ParseMode.MARKDOWN_V2,
+        open_period=50,
+        reply_markup=markup3,
+    )
+
+@dp.callback_query_handler(lambda func: func.data == 'button5')
+async def solve_task(call: types.CallbackQuery):
+    answer4 = ['type error','0','1','-1','None']
+    question4 = 'print((lambda x: x**2 + 5*x + 4) (-4))'
+    button6 = InlineKeyboardButton('Next_task', callback_data='button6')
+    markup4 = InlineKeyboardMarkup()
+    markup4.add(button6)
 
 
-start()
+    await bot.send_poll(
+        call.message.chat.id,
+        question=question4,
+        options=answer4,
+        correct_option_id=1,
+        is_anonymous=True,
+        type='quiz',
+        explanation='only one of these options are true',
+        explanation_parse_mode=ParseMode.MARKDOWN_V2,
+        open_period=50,
+        reply_markup=markup4,
+    )
+
+@dp.callback_query_handler(lambda func: func.data == 'button6')
+async def solve_task(call: types.CallbackQuery):
+    answer4 = ['None','KeyError','0']
+    question4 = 'test = {}  ' \
+                'print(test[0])'
+
+    await bot.send_poll(
+        call.message.chat.id,
+        question=question4,
+        options=answer4,
+        correct_option_id=1,
+        is_anonymous=True,
+        type='quiz',
+        explanation='only one of these options are true',
+        explanation_parse_mode=ParseMode.MARKDOWN_V2,
+        open_period=50,
+    )
+
+# -------------------------------------------------------------------------------
+
+
+@dp.callback_query_handler(lambda func: func.data == 'button1')
+async def solve_task(call: types.CallbackQuery):
+    photo3 = open('../media/home_work_solved/photo5278541801109700202.jpg', 'rb')
+    answer4 = ['A','B', 'C', 'All options are not correct', 'All options are correct','None']
+    question4 = 'Output of this task in picture above,(WARNING, input lists have given for all task option):'
+
+    await bot.send_photo(
+        call.message.chat.id,
+        photo=photo3)
+
+    await bot.send_poll(
+        call.message.chat.id,
+        question=question4,
+        options=answer4,
+        correct_option_id=4,
+        is_anonymous=False,
+        type='quiz',
+        explanation='only one of these options are true',
+        explanation_parse_mode=ParseMode.MARKDOWN_V2,
+        open_period=50,
+    )
+
+
+@dp.message_handler()
+async def echo_message(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=False)
