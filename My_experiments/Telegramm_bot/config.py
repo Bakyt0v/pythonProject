f15747ee8d43230498a93195312d277d52c6543f
Bakyt0v <Amantur470@gmil.com>
2022-03-09 22:19:05 +0600

    Delete hw.db

--- My_experiments/Telegramm_bot/config.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Bot, executor, Dispatcher, types
-from aiogram.types import base,\
-    InlineKeyboardButton, InlineKeyboardMarkup
-from decouple import config
-
-
-TOKEN = config("TOKEN")
-bot = Bot(TOKEN)
-dp = Dispatcher(bot=bot)
